d09b0e2594b791bf16f9d87dd22dc5dd6f08d3f7
nxdannik <sinkevhcdavid@gmail.com>
2024-11-03 22:24:04 +0200

    Add files via upload

---
+++ Praktychnna1.py
@@ -0,0 +1,8 @@
+a = 'Hello world'
+name = 'Danilo'
+surname = 'Sinkevich'
+age = 16
+print('Value а:', a ,'Type:', type(a))
+print('Value name:', name, 'Type:', type(name))
+print('Value surname:', surname,'Type:', type(surname))
+print('Value age:', age, 'Type:', type(age))
